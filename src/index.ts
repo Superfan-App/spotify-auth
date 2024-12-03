8c6da5e2362549d867829d78eb540df04b1913e5
will <39891237+william-matz@users.noreply.github.com>
2024-12-03 16:28:07 -0500

    remove unnecessary examples

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-// Reexport the native module. On web, it will be resolved to SpotifyOAuthModule.web.ts
-// and on native platforms to SpotifyOAuthModule.ts
 export { default } from "./SpotifyOAuthModule";
 export * from "./SpotifyOAuth.types";
