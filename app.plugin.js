cc1443eca63df86b37d666cdd03f10058afff7d2
will <39891237+william-matz@users.noreply.github.com>
2025-02-04 12:19:32 -0500

    update config plugin configuration

--- app.plugin.js
+++ app.plugin.js
@@ -1,1 +1,6 @@
-module.exports = require('./plugin/build');
+const { withPlugins } = require('@expo/config-plugins');
+const withSpotifyConfig = require('./plugin/build/withSpotifyConfig').default;
+
+module.exports = (config) => {
+  return withPlugins(config, [withSpotifyConfig]);
+};
